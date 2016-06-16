dc1951cbbcc5df1681e65407b332d3d714c03267
k0rsh <korsh@yandex.ru>
2016-06-17 00:51:18 +0300

    Added layouts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
 
+	def index
+		@articles = Article.all
+	end
+
 	def new
 		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])  # prepare @article variable for the 'edit' template
 	end
 
 	def create
@@ -18,9 +26,21 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			flash[:notice] = "Article update succeded"
+			redirect_to article_path(@article)	# show the edited article
+		else
+			render :edit  # render edit template once again if validation failed
+		end
+	end
+
 	def show
 		@article = Article.find(params[:id])	# find article in table Article by :id passed via GET request param (article/:id)
 	end
+
 
 private
 	def article_params
